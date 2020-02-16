ee099cc4917629cd30c5c4cf18800c1cfcc927e2
Underslash12 <stew_smith@hotmail.com>
2020-02-15 20:10:20 -0800

    Status Bars done

---
+++ Assets/Scripts/textController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text distance;
+    public Text bloodSugar;
+    private float bsNum;
+    public Text fuel;
+    private float fuelNum;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+
+        rb = GameObject.Find("player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance.text = "Distance: " + 
+            (int)(rb.position.x * 10) / 10f + "m"; 
+        bloodSugar.text = "BS: " + (int)(bsNum * 10) / 10f + "%";
+        fuel.text = "Fuel: " + (int)(fuelNum * 10) / 10f + "%";
+    }
+
+    void updateBS (float bs)
+    {
+        bsNum += bs;
+    }
+
+    void updateFuel (float fl)
+    {
+        fuelNum += fl;
+    }
+}
