fb228aed84e6441d90be7f4ae1191c52285c85c5
Minnowfeather <Minnowfeather@github.com>
2020-02-15 19:20:11 -0800

    Implemented screen changer

---
+++ Assets/Scripts/cameraFollowObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowObject : MonoBehaviour
+{
+
+    // ToDO:Make modular
+
+    private GameObject followTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followTarget = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(followTarget.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = pos;
+    }
+}
