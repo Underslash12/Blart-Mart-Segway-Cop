ac4d62633213af1f4991836b7d83529a569fc2a6
Underslash12 <stew_smith@hotmail.com>
2020-02-16 02:49:54 -0800

    Merged properly?

---
+++ Assets/Scripts/collectableCollector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableCollector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == "candy"){
+            GameObject.FindWithTag("Player").GetComponent<textController>().updateBS(10);
+        }
+        if(collider.gameObject.tag == "fuel"){
+            GameObject.FindWithTag("Player").GetComponent<textController>().updateFuel(10);
+        }
+        Destroy(collider.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
